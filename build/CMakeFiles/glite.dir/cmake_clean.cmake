e2f7319fc1abfbca69c7752c95163eed9915ace7
navY <pyvan223@gmail.com>
2026-06-30 14:26:02 +0000

    Module 0: toolchain setup, GitLite prints ready message

---
+++ build/CMakeFiles/glite.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glite.dir/link.d"
+  "CMakeFiles/glite.dir/src/main.c.o"
+  "CMakeFiles/glite.dir/src/main.c.o.d"
+  "glite"
+  "glite.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("GitLite Toolchain Ready.\n");
+    return 0;
+}
